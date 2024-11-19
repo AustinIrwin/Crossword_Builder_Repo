ee4d0fb82b555289925b7b751399be175c4e3e67
Austin Irwin <austindirwin@gmail.com>
2024-11-18 22:09:04 -0500

    Created a crossword builder project

---
+++ src/main/java/org/crosswordBuilder/Main.java
@@ -0,0 +1,8 @@
+package org.crosswordBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        System.out.println("This is basically a crossword builder lol");
+    }
+}
